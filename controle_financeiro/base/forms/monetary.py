3c432a2d543f61b7a2f7c8f1c08fc0a478d9a3ed
Douglas Panhota <douglaspands@gmail.com>
2021-04-12 14:41:05 -0300

    MonetaryField: Valor maximo de 10 quatrilhoes - 1.

--- controle_financeiro/base/forms/monetary.py
+++ controle_financeiro/base/forms/monetary.py
@@ -14,14 +14,14 @@ locale.setlocale(locale.LC_MONETARY, settings.DEFAULT_LOCALE)
     re.findall(r"[.,]", locale.currency(1234.56, grouping=True, symbol=False))
 )
 
-ECMA_ONKEYUP = (r'(e=>{const s=e.value.match(/[0-9]/g).join("");if(s){const[t,i]=(parseInt(s)/100).toFixed(2).split("."),u=t.split("").reverse().reduce((e,s,t)=>(t<=1?e.push(s):(t+1)%3==1?e.push(`${s}THOUSAND_POINT`):e.push(s),e),[]).reverse().join("");e.value=`${u}DECIMAL_POINT${i}`}else e.value=""})(this);'
+ECMA_ONKEYUP = (r'(e=>{const s=e.value.match(/[0-9]/g).join("");if(s){const[t,i]=(parseInt(s, 10)/100).toFixed(2).split("."),u=t.split("").reverse().reduce((e,s,t)=>(t<=1?e.push(s):(t+1)%3==1?e.push(`${s}THOUSAND_POINT`):e.push(s),e),[]).reverse().join("");e.value=`${u}DECIMAL_POINT${i}`}else e.value=""})(this);'
                 .replace("THOUSAND_POINT", THOUSAND_POINT)
                 .replace("DECIMAL_POINT", DECIMAL_POINT))
 
 
 class MonetaryField(forms.Field):
     def __init__(self, *args, **kwargs):
-        kwargs["widget"] = forms.TextInput(attrs={"onkeyup": ECMA_ONKEYUP})
+        kwargs["widget"] = forms.TextInput(attrs={"onkeyup": ECMA_ONKEYUP, "maxlength": 20})
         super().__init__(*args, **kwargs)
 
     def prepare_value(self, value: Optional[Decimal]) -> str:
